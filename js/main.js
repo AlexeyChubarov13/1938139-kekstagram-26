75b2850bdfbff0358059a791c4564d6203cffc06
Алексей <itachi13aaaa@gmail.com>
2022-07-16 16:11:05 +0300

    Добавляет вспомогательные функции

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function randomNumber(min, max) {
+  min = Math.min(Math.abs(min), Math.abs(max));
+  max = Math.max(Math.abs(min), Math.abs(max));
+  return Math.round(Math.random() * (max - min) + min);
+}
+randomNumber(0, 2);
+
+
+const stringLength = function(stringnumber, maxstring) {
+  return stringnumber.length <= maxstring;
+};
+
+
+stringLength('Привет мир', 7);
